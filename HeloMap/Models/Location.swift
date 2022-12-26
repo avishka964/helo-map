47348330daeb2c8e10dda2a61e52c7128d2dd5b8
Avishka <avishkakapuruge@gmail.com>
2022-12-26 19:35:25 +0530

    update L-1

---
+++ HeloMap/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  HeloMap
+//
+//  Created by Avishka Kapuruge on 2022-12-26.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
