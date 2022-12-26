86acacd8b0d1498fdab02486eb3acab539306566
Avishka <avishkakapuruge@gmail.com>
2022-12-26 18:31:52 +0530

    Initial Commit

---
+++ HeloMap/HeloMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeloMapApp.swift
+//  HeloMap
+//
+//  Created by Avishka Kapuruge on 2022-12-26.
+//
+
+import SwiftUI
+
+@main
+struct HeloMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
